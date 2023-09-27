c7c17781c8ac99194332d97bdae78affbd0b9788
Hien Nguyen <hien.nguyen@deliveree.com>
2023-09-27 09:39:08 +0700

    Update login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as yup from 'yup'
 
 import React from 'react'
@@ -10,8 +11,8 @@ import Input from 'pages/components/common/Input'
 
 const loginSchema = () =>
   yup.object({
-    account: yup.string().required('Required Account!'),
-    password: yup.string().required('Required Password!'),
+    account: yup.string().required('Account is required!'),
+    password: yup.string().required('Password is required!'),
   })
 
 const Login = () => {
@@ -25,20 +26,37 @@ const Login = () => {
   const onSubmit = (data: any) => console.log(data)
 
   return (
-    <div className="container ">
+    <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Code Challenge</a>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div className="container mt-5">
       <div className="row justify-content-center">
-        <div className="col-md-4">
+        <div className="col-md-4 border p-4 rounded">
           <h1>Login</h1>
           <FormProvider {...methods}>
             <Form onSubmit={methods.handleSubmit(onSubmit)}>
               <Input name={'account'} label={'Account'} />
-              <Input name={'password'} label={'Password'} />
+              <Input name={'password'} label={'Password'} inputType='password' />
               <Button type="submit">Confirm</Button>
             </Form>
           </FormProvider>
         </div>
       </div>
-    </div>
+      </div>
+      </>
   )
 }
 
